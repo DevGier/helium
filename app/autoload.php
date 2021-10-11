3146f64ea6903cc7515ecc41ba64aa75ad814fec
Tim de Gier <45061164+timdegier@users.noreply.github.com>
2021-10-11 17:17:49 +0200

    Update autoload.php

--- app/autoload.php
+++ app/autoload.php
@@ -104,6 +104,15 @@ session_start();
 ob_start();
 
 /**
+ * Require vendor autoload
+ */
+if(file_exists(__DIR__ . '/../vendor/autoload.php')){
+
+  require_once(__DIR__ . '/../vendor/autoload.php');
+
+}
+
+/**
  * Autoload configs
  */
 autoload(__DIR__ . '/../config/');
